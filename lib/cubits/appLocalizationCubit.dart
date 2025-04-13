781a2217895dd4f26fd493e8970662f4292aed68
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-13 18:35:34 +0200

    feat:update commit, please review  commit 2025-04-13 18:35:33

--- lib/cubits/appLocalizationCubit.dart
+++ lib/cubits/appLocalizationCubit.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get/get.dart';
 import 'package:nexus/data/repositories/settingsRepository.dart';
 import 'package:nexus/utils/utils.dart';
 
