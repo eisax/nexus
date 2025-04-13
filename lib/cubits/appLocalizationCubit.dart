52f1ece79cf0590d78d0db20761063bb6d6d6041
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-13 17:13:24 +0200

    feat:update commit, please review  commit 2025-04-13 17:13:24

--- lib/cubits/appLocalizationCubit.dart
+++ lib/cubits/appLocalizationCubit.dart
@@ -1,10 +1,3 @@
 import 'package:flutter/material.dart';
 
-class AppLocalizationState extends StatelessWidget {
-  const AppLocalizationState({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
+cla
