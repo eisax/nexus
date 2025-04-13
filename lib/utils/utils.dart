0f19ea57b24f1f5afbd8a45acca938efaf449837
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-13 17:18:52 +0200

    feat:update commit, please review  commit 2025-04-13 17:18:52

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,3 +1,8 @@
 class Utils{
-  
+  static Locale getLocaleFromLanguageCode(String languageCode) {
+    List<String> result = languageCode.split("-");
+    return result.length == 1
+        ? Locale(result.first)
+        : Locale(result.first, result.last);
+  }
 }
