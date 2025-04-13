1971404da7bae532dbd72b66d86a1fb5c657a2f2
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-13 17:03:41 +0200

    feat:update commit, please review  commit 2025-04-13 17:03:41

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -41,7 +41,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [],
-      child: ,
+      child: Builder(builder: ),
     );
   }
 }
