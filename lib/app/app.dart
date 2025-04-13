190a4d1dea3915b11918051b96275d03fd413c2b
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-13 16:00:46 +0200

    feat:update commit, please review  commit 2025-04-13 16:00:46

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -22,3 +22,18 @@ Future<void> initializeApp() async {
 
   runApp(const MyApp());
 }
+
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
