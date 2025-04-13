fe8e68acfc350eae736294ba1b1727036886e68a
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-13 17:42:55 +0200

    feat:update commit, please review  commit 2025-04-13 17:42:55

--- lib/ui/screens/splashScreen.dart
+++ lib/ui/screens/splashScreen.dart
@@ -14,8 +14,6 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: C,
-    );
+    return const Scaffold(body: Center(child: Text("Splash")));
   }
 }
