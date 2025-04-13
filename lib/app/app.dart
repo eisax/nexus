6f9ee131480272a0ecd824c12e8790f72c51b2ef
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-13 17:03:21 +0200

    feat:update commit, please review  commit 2025-04-13 17:03:21

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 Future<void> initializeApp() async {
   WidgetsFlutterBinding.ensureInitialized();
