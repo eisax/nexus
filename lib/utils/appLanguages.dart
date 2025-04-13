fffaaf5b5d63d97ec12936c7dc5af55fe6b97cf5
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-13 17:29:03 +0200

    feat:update commit, please review  commit 2025-04-13 17:29:03

--- lib/utils/appLanguages.dart
+++ lib/utils/appLanguages.dart
@@ -4,4 +4,5 @@ const String defaultLanguageCode = "en";
 
 const List<AppLanguage> appLanguages = [
   AppLanguage(languageCode: "en", languageName: "English"),
+  AppLanguage(languageCode: "sh", languageName: "Shona"),
 ];
