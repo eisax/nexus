d030e4bb0ae22a39cb8521a04cd69d4b28528df2
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-13 17:09:00 +0200

    feat:update commit, please review  commit 2025-04-13 17:09:00

--- lib/ui/styles/colors.dart
+++ lib/ui/styles/colors.dart
@@ -4,3 +4,6 @@ const Color primaryColor = Color(0xff22577A);
 const Color secondaryColor = Color(0xff212121);
 const Color pageBackgroundColor = Color(0xffFFFFFF);
 const Color backgroundColor = Color(0xfff6f6f6);
+
+const Color errorColor = Color(0xffff6769);
+const Color onBackgroundColor = Color(0xff484848);
