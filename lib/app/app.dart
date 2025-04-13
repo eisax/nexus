a3cc220c082cef9c041a4041b21794da937b03da
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-13 16:01:48 +0200

    feat:update commit, please review  commit 2025-04-13 16:01:48

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -34,6 +34,10 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold();
+    return  Scaffold(
+      body: Center(
+        child: Text(""),
+      ),
+    );
   }
 }
