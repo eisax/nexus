ae0d5cc11f4694d8c70c8de8b3fbb7c14417395b
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-13 17:22:26 +0200

    feat:update commit, please review  commit 2025-04-13 17:22:26

--- lib/data/repositories/settingsRepository.dart
+++ lib/data/repositories/settingsRepository.dart
@@ -1,3 +1,5 @@
+import 'package:hive_flutter/hive_flutter.dart';
+
 class SettingsRepository {
   Future<void> setCurrentLanguageCode(String value) async {
     Hive.box(settingsBoxKey).put(currentLanguageCodeKey, value);
