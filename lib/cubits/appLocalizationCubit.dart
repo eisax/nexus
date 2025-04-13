b6e95c40b5fbe6b83fc4a9c6a188b6715d8f34bf
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-13 17:15:08 +0200

    feat:update commit, please review  commit 2025-04-13 17:15:08

--- lib/cubits/appLocalizationCubit.dart
+++ lib/cubits/appLocalizationCubit.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AppLocalizationState {
   final Locale language;
@@ -6,5 +7,5 @@ class AppLocalizationState {
 }
 
 
-class AppLocalizationCubit extends Cubic<AppLocalizationState>{}
+class AppLocalizationCubit extends Cubit<AppLocalizationState>{}
 
