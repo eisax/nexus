65041769a08e3241c6ef354c0818e108aef6eb42
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-13 17:43:02 +0200

    feat:update commit, please review  commit 2025-04-13 17:43:02

--- lib/ui/screens/home/homeScreen.dart
+++ lib/ui/screens/home/homeScreen.dart
@@ -14,6 +14,6 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+      return const Scaffold(body: Center(child: Text("Splash")));
   }
 }
