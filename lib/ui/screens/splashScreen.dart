b39ba7db26b8ff624f880e497d9547e8b06719ca
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-13 19:46:58 +0200

    feat:update commit, please review  commit 2025-04-13 19:46:58

--- lib/ui/screens/splashScreen.dart
+++ lib/ui/screens/splashScreen.dart
@@ -19,7 +19,7 @@ class _SplashScreenState extends State<SplashScreen> {
 
   @override
   void initState() {
-    // TODO: implement initState
+    navigateToNextScreen();
     super.initState();
   }
 
