111fe2f793245aea8b1a2c5a13182b2260fb6b91
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-13 18:43:48 +0200

    feat:update commit, please review  commit 2025-04-13 18:43:48

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -28,7 +28,7 @@ class MyHttpOverrides extends HttpOverrides {
 
 Future<void> initializeApp() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  HttpOverrides.global = MyHttpOverrides();
   LicenseRegistry.addLicense(() async* {
     final license = await rootBundle.loadString('google_fonts/OFL.txt');
     yield LicenseEntryWithLineBreaks(['google_fonts'], license);
