35e9842d1784078f13b9a036252f8144b4e7a21a
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-13 17:28:56 +0200

    feat:update commit, please review  commit 2025-04-13 17:28:56

--- lib/utils/appLanguages.dart
+++ lib/utils/appLanguages.dart
@@ -1,10 +1,7 @@
-
-
 import 'package:nexus/data/models/appLanguage.dart';
 
 const String defaultLanguageCode = "en";
 
-const List<AppLanguage> appLanguages [
+const List<AppLanguage> appLanguages = [
   AppLanguage(languageCode: "en", languageName: "English"),
 ];
-
