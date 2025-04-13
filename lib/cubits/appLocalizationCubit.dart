e3f31bd32f618998f7e6f6b1b73200ea380615bc
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-13 17:19:10 +0200

    feat:update commit, please review  commit 2025-04-13 17:19:10

--- lib/cubits/appLocalizationCubit.dart
+++ lib/cubits/appLocalizationCubit.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nexus/data/repositories/settingsRepository.dart';
+import 'package:nexus/utils/utils.dart';
 
 class AppLocalizationState {
   final Locale language;
