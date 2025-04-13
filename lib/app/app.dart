134a2135cd768b62d1eba9bb2f1565d58d3ed0d6
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-13 17:10:33 +0200

    feat:update commit, please review  commit 2025-04-13 17:10:33

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -57,6 +57,7 @@ class _MyAppState extends State<MyApp> {
                  primary: primaryColor,
                   onPrimary: onPrimaryColor,
                    secondary: secondaryColor,
+                   surface: backgroundColor,
               )
             ),
           );
