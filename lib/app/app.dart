48a4c4a511cf364c17557b1248695ad6365524ca
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-13 15:57:54 +0200

    feat:update commit, please review  commit 2025-04-13 15:57:53

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -2,4 +2,9 @@ import 'package:flutter/material.dart';
 
 Future<void> initializeApp() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('google_fonts/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
 }
