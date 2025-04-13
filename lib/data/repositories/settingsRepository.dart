42b695172f7120203a48ed6128df05dcdfdad755
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-13 17:19:32 +0200

    feat:update commit, please review  commit 2025-04-13 17:19:32

--- lib/data/repositories/settingsRepository.dart
+++ lib/data/repositories/settingsRepository.dart
@@ -1,2 +1,5 @@
-
-class SettingsRepository{}
+class SettingsRepository {
+  Future<void> setCurrentLanguageCode(String value) async {
+    Hive.box(settingsBoxKey).put(currentLanguageCodeKey, value);
+  }
+}
